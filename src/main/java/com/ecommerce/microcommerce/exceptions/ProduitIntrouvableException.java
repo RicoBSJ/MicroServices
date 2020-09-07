848ad28e0a14eb2bba35578a640420c2eeda382d
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2020-09-07 23:13:58 +0200

    ErrorMessageButAlwaysPb

--- src/main/java/com/ecommerce/microcommerce/exceptions/ProduitIntrouvableException.java
+++ src/main/java/com/ecommerce/microcommerce/exceptions/ProduitIntrouvableException.java
@@ -6,5 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProduitIntrouvableException extends Throwable {
     public ProduitIntrouvableException(String s) {
+
+        super(s);
     }
 }
