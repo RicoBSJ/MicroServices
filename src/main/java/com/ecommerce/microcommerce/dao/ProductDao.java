f0e190a538ec5886e78b44cf6300c9a78d2a2bf8
AUBRUNE <e-aubrun@ari.asso.fr>
2020-09-07 15:19:56 +0200

    ModifyPropertiesAndQuery

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -15,7 +15,7 @@ public interface ProductDao extends JpaRepository<Product, Integer> {
 
     List<Product> findByPrixGreaterThan(int prixLimit);
 
-    @Query("select p from Product p where p.prix > :prixLimit")
+    @Query("select id, nom, prix from Product where prix > :prixLimit")
     List<Product> chercherUnProduitCher(@Param("prixLimit") int prix);
 
 }
